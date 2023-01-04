f1266b6481bcf7d6768d519c0d1d0fff60e7c6b1
eeseung <seunghee9941@gmail.com>
2023-01-04 17:05:21 +0900

    [PRG(style)] Level1: K번째수 :label: 정렬

--- Programmers/Level1/K번째수.js
+++ Programmers/Level1/K번째수.js
@@ -2,7 +2,7 @@ function solution(array, commands) {
   const answer = [];
 
   for (const c of commands) {
-      answer.push(array.slice(c[0] - 1, c[1]).sort((a, b) => a - b)[c[2] - 1]);
+    answer.push(array.slice(c[0] - 1, c[1]).sort((a, b) => a - b)[c[2] - 1]);
   }
 
   return answer;
