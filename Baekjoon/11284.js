5ae219d302dbb156df47fa0b8bf9b87c6e009d9e
eeseung <seunghee9941@gmail.com>
2023-03-25 20:17:44 +0900

    :sparkles: [BOJ] 11284 초성 중성 종성 | B2 | 문자열

---
+++ Baekjoon/11284.js
@@ -0,0 +1,21 @@
+// 초성 중성 종성
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'test.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+function solution(str) {
+  const FIRST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+  const MIDDLE = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
+  const LAST = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+
+  const char = str.charCodeAt(0) - 44032;
+  const lastIdx = char % 28;
+  const middleIdx = ((char - lastIdx) / 28) % 21;
+  const firstIdx = (((char - lastIdx) / 28) - middleIdx) / 21;
+
+  return [FIRST[firstIdx], MIDDLE[middleIdx], LAST[lastIdx]].join('\n');
+}
+
+console.log(solution(input[0]));
