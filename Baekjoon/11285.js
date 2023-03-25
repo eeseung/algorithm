01e41ace65233d6812608f523a7142ca3e580f40
eeseung <seunghee9941@gmail.com>
2023-03-25 20:18:22 +0900

    :sparkles: [BOJ] 11285 초성 중성 종성 2 | B1 | 문자열

---
+++ Baekjoon/11285.js
@@ -0,0 +1,20 @@
+// 초성 중성 종성 2
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'test.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+function solution(arr) {
+  const FIRST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+  const MIDDLE = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'];
+  const LAST = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+
+  const firstIdx = FIRST.indexOf(arr[0]);
+  const middleIdx = MIDDLE.indexOf(arr[1]);
+  const lastIdx = LAST.indexOf(arr[2] || '');
+
+  return String.fromCharCode(firstIdx * 21 * 28 + middleIdx * 28 + lastIdx + 44032);
+}
+
+console.log(solution(input));
