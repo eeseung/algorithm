b3f3478729a7d9f8c7b37a638225700ef634c0ae
eeseung <seunghee9941@gmail.com>
2023-05-25 23:57:23 +0900

    :recycle: [BOJ] 10844 쉬운 계단 수

--- Baekjoon/10844.js
+++ Baekjoon/10844.js
@@ -5,14 +5,13 @@ const input = require('fs')
   .trim()
   .split('\n');
 
-function solution(n) {
-  const dp = Array.from({length: n + 1}, () => Array(10).fill(0));
+function solution(N) {
+  const dp = Array.from({length: N + 1}, () => Array(10).fill(0));
 
   // dp[n][i] -> 길이가 n이면서 i로 끝나는 계단 수의 개수
   dp[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  dp[2] = [1, 1, 2, 2, 2, 2, 2, 2, 2, 1];
 
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i <= N; i++) {
     for (let j = 0; j < 10; j++) {
       if (j === 0) {
         dp[i][j] = dp[i - 1][j + 1] % 1000000000;
@@ -24,7 +23,7 @@ function solution(n) {
     }
   }
 
-  return dp[n].reduce((acc, cur) => acc + cur, 0) % 1000000000;
+  return dp[N].reduce((acc, cur) => acc + cur, 0) % 1000000000;
 }
 
 console.log(solution(parseInt(input[0])));
