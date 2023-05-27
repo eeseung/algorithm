3fd4c5e02b3b5551b4eb74259d4760ced92d23fb
eeseung <seunghee9941@gmail.com>
2023-05-27 19:33:00 +0900

    :sparkles: [PRG] 방금그곡 | lv2 | 구현

---
+++ Programmers/Level2/방금그곡.js
@@ -0,0 +1,36 @@
+function getPitches(str) {
+  const result = [];
+  
+  for (const s of str.split('')) {
+    if (s === '#') result[result.length - 1] = result[result.length - 1] + '#';
+    else result.push(s);
+  }
+  
+  return result;
+}
+
+function solution(m, musicinfos) {
+  const mArr = getPitches(m);
+  const answer = [];
+  
+  for (const music of musicinfos) {
+    const [start, end, title, pitch] = music.split(',');
+    const startTime = start.split(':').map(Number);
+    const endTime = end.split(':').map(Number);
+    const time = Math.abs(endTime[0] * 60 + endTime[1] - startTime[0] * 60 - startTime[1]);      const allPitchArr = getPitches(pitch.repeat(Math.ceil(time / pitch.replace('#', '').length))).slice(0, time);
+    let idx = 0;
+      
+    for (let i = 0; i < allPitchArr.length; i++) {
+      if (allPitchArr[i] === mArr[idx]) {
+        if (idx === mArr.length - 1) {
+          answer.push({title: title, duration: time, index: answer.length});
+          break;
+        }
+        idx++;
+      } else if (allPitchArr[i] === mArr[0]) idx = 1;
+      else idx = 0;
+    }
+  }
+  
+  return answer.length > 0 ? answer.sort((a, b) => a.duration === b.duration ? a.index - b.index : b.duration - a.duration)[0].title : '(None)';
+}
