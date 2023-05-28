8e590042f9e7abdc6bcb3a63f65019ee95e5d51f
eeseung <seunghee9941@gmail.com>
2023-05-28 22:28:40 +0900

    :sparkles: [PRG] 방금그곡 | lv2 | 구현

--- Programmers/Level2/방금그곡.js
+++ Programmers/Level2/방금그곡.js
@@ -31,6 +31,26 @@ function solution(m, musicinfos) {
       else idx = 0;
     }
   }
+}
+
+// 다른 풀이: C#, D#, F#, G#, A# -> 다른 문자로 바꾸기
+function normalize(melody) {
+  return melody.replace(/([A-Z])#/g, (_, a) => a.toLowerCase());
+}
+
+function solution(m, musicinfos) {
+  const melody = normalize(m);
+  const answer = [];
+  
+  for (const music of musicinfos) {
+    const [start, end, title, pitch] = music.split(',');
+    const startTime = start.split(':').map(Number);
+    const endTime = end.split(':').map(Number);
+    const time = Math.abs(endTime[0] * 60 + endTime[1] - startTime[0] * 60 - startTime[1]);
+    const pitches = normalize(pitch.repeat(Math.ceil(time / pitch.replace('#', '').length))).slice(0, time);
+      
+    if (pitches.includes(melody)) answer.push({title: title, duration: time, index: answer.length});
+  }
   
   return answer.length > 0 ? answer.sort((a, b) => a.duration === b.duration ? a.index - b.index : b.duration - a.duration)[0].title : '(None)';
 }
