9366634d315269b47f351959a23666220f755c40
Seunghee Lee <49032882+eeseung@users.noreply.github.com>
2025-04-23 23:54:19 +0900

    [Gold IV] Title: 수들의 합 4, Time: 332 ms, Memory: 41056 KB -BaekjoonHub

---
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.js
@@ -0,0 +1,29 @@
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'test.txt')
+  .toString()
+  .trim()
+  .split('\n');
+const [N, K] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+
+function solution(N, K, arr) {
+  const sum = Array.from({length: N + 1}).fill(0);
+  const obj = {};
+  let answer = 0;
+
+  for (let i = 1; i <= N; i++) {
+    sum[i] = sum[i - 1] + arr[i - 1];
+    if (sum[i] === K) answer++;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (obj[sum[i] - K]) answer += obj[sum[i] - K];
+    
+    if (obj[sum[i]]) obj[sum[i]]++;
+    else obj[sum[i]] = 1;
+  }
+
+  return answer;
+}
+
+console.log(solution(N, K, arr));
